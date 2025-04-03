81b013af4ce9a6faba9d6c29a1466a540e51481b
zthuii <63137661+zthuii@users.noreply.github.com>
2025-04-03 07:08:45 +0000

    Deploying to gh-pages from @ zthuii/nav@2bd7e319fbf3c97b10802210d24b0b4621e2c400 🚀

---
+++ chunk-Y2DFP4OB.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-ZKCMIIXD.js";import{Ab as e,qb as r}from"./chunk-6A54WZOQ.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
